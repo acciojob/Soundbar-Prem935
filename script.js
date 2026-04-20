21d79e7f4d57cb4daee38bc9af2b4c85d947a0ae
Prem935 <prem886044@gmail.com>
2026-04-21 00:21:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const buttonContainer = document.querySelector('#buttons')
+const buttonNames = ['applause','boo','gasp','tada','victory','wrong']
+
+buttonNames.map(item => {
+	const button = document.createElement('button')
+	button.innerText = item
+	button.className = "btn"
+	buttonContainer.appendChild(button)
+})
